4d18dfe27a94f8d6f8fc589db1acd537791cef12
Frotten <frottenice@outlook.com>
2025-04-28 20:39:51 +0800

    无

--- Indefinite_parameter.go
+++ Indefinite_parameter.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 )
 
-func Add(Arr ...int) (sum int) {
+func Add01(Arr ...int) (sum int) {
 	for _, value := range Arr {
 		sum += value
 	}
 	return sum
 }
+
+func Add02(Arr []int) (sum int) {
+	for _, value := range Arr {
+		sum += value
+	}
+	return
+}
 func main() {
 	Arr := []int{2, 3, 6, 9, 5}
-	fmt.Println("Sum :", Add(Arr...))
+	fmt.Println("Sum :", Add01(Arr...))
+	fmt.Printf("%T\n", Add01)
+	fmt.Printf("%T\n", Add02)
 }
